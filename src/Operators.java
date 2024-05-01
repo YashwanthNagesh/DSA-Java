0a3352f3d39b39851458ebfc0993ac198373c965
YashwanthNagesh <yashwanth.softeng02@gmail.com>
2024-05-01 18:35:45 +0530

    Initial commit

---
+++ src/Operators.java
@@ -0,0 +1,7 @@
+public class Operators {
+    public static void main(String[] args){
+        int firstValue = 5;
+        int secondValue = 6;
+        System.out.println(firstValue+secondValue);
+    }
+}
