edca9f923388be0147b24198b1ac7d3ef5f832b5
YashwanthNagesh <yashwanth.softeng02@gmail.com>
2024-05-02 21:16:47 +0530

    Solved Operator Questions

---
+++ src/Basics1.java
@@ -0,0 +1,9 @@
+public class Basics1 {
+
+    public static void main(String[] args){
+        System.out.print(100);
+        System.out.print(50);
+        System.out.println(80);
+        System.out.print(60);
+    }
+}
